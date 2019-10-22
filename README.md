5999fadd182675c6c6e80696481a66be973b2891
codydegen <codydegen@gmail.com>
2019-10-22 15:18:30 -0400

    Add GUI and related functions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RockPaperScissors
-TOP
+
+Project for The Odin Project.  Purpose is to write a GUI and JavaScript functions to show understanding of the DOM.
--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,47 @@
+const buttons = document.querySelectorAll('button');
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
+const result = document.querySelector(".result");
+console.log(playerScore);
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const playChoice = e.target.firstChild.data.toLowerCase();
+        console.log(playChoice);
+        let x = playRound(playChoice, computerPlay());
+        console.log(x);
+        if (playerScore.textContent >= 5 || computerScore.textContent >= 5){
+            result.textContent="Game Over!";
+            if (playerScore.textContent >= 5){
+                result.textContent+=" You Win!";
+            }else {
+                result.textContent+=" You Lose.";
+            }
+        }else if (playerScore.textContent == 4 && x == 1){
+            playerScore.textContent++;
+            result.textContent+="  Game Over!";
+        }else if(computerScore.textContent == 4 && x === -1){
+            computerScore.textContent++;
+            result.textContent+="  Game Over!";
+        }else if (x==1){
+            playerScore.textContent++;
+        }else if (x== -1 ){
+            computerScore.textContent++;
+        }
+
+    });
+});
+
+function scoreKeep(result, pScore, cScore){ 
+    //let pScore = 0, cScore = 0;
+    if (result == 1){
+        pScore++;
+    }else if (result == -1){
+        cScore++;
+    }
+        console.log("Score: Player: " + pScore + " Computer: " + cScore)
+    }
+
+
 function computerPlay(){
     let choice = Math.random();
     console.log( choice );
@@ -13,17 +57,19 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection,computerSelection);
 if (playerSelection=="rock" && computerSelection=="scissors" || playerSelection=="scissors" && computerSelection=="paper" || playerSelection=="paper" && computerSelection=="rock") {
-    console.log("You Win! " + playerSelection[0].toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".");
+    result.textContent = "You Win! " + playerSelection[0].toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ".";
     return 1;
 } else if (playerSelection=="scissors" && computerSelection=="rock" || playerSelection=="paper" && computerSelection=="scissors" || playerSelection=="rock" && computerSelection=="paper") {
-    console.log("You Lose! " + computerSelection[0].toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + ".");
+    result.textContent = "You Lose! " + computerSelection[0].toUpperCase() + computerSelection.slice(1) + " beats " + playerSelection + ".";
     return -1;
 } else {
-    console.log("You tied.  You both picked " + computerSelection[0].toUpperCase() + computerSelection.slice(1) + ".");
+    result.textContent = "You tied.  You both picked " + computerSelection[0].toUpperCase() + computerSelection.slice(1) + ".";
     return 0;
 }
     
 }
+
+
 function game(){
     let pScore = 0, cScore = 0, x = 0;
     while(pScore < 3 && cScore < 3){
@@ -54,4 +100,4 @@ function playerChoose(){
     }
     }
 }
-game();
+//game();
